b2864245074fd32d870cf8662076dcf3808e3579
pond823 <sasha.bilton@buzzbomb.org>
2013-10-14 12:35:53 +0100

    Cleaned up const & variable names, added comments.

--- z80Test/z80Test.ino
+++ z80Test/z80Test.ino
@@ -2,31 +2,38 @@
 
 // constants won't change. Used here to 
 // set pin numbers:
-const int ledPin =  13;      // the number of the LED pin
-const int resetPin = 12;
-// Variables will change:
+const int CLOCK_PIN =  13;      // the number of the LED pin
+const int RESET_PIN = 12;
 const int DATA_PINS = 30;
+const int ADDRESS_LOW = 2;
+const int ADDRESS_HIGH = 40;
 
-int ledState = LOW;             // ledState used to set the LED
-long previousMillis = 0;        // will store last time LED was updated
-
-
+int clockState = LOW;             // clockState used to set the CLK
 
 // the follow variables is a long because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
-long interval = 10;           // interval at which to blink (milliseconds)
-int addressPin =0;
+long interval = 10;           // clock interval (milliseconds)
+long previousMillis = 0;        // will store last time LED was updated
+
 
 void setup() {
-  // set the digital pin as output:
-   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT); 
-  for (addressPin=2; addressPin<10; addressPin++) {
+  
+  int addressPin;
+ 
+  Serial.begin(9600);
+  
+  //Set the CLK pin to OUTPUT
+  pinMode(CLOCK_PIN, OUTPUT); 
+  
+  //Set the address pins to INPUT
+  for (addressPin=ADDRESS_LOW; addressPin<ADDRESS_LOW+8; addressPin++) {
     pinMode(addressPin, INPUT);  
   }
-    for (addressPin=40; addressPin<48; addressPin++) {
+    for (addressPin=ADDRESS_HIGH; addressPin<ADDRESS_HIGH+8; addressPin++) {
     pinMode(addressPin, INPUT);  
   }
+  
+  //Set the DATA pins to output and set to LOW so default operation is NOP
   for (int data=DATA_PINS; data < 9; data++) {
     pinMode(data, OUTPUT);
     digitalWrite(data, LOW);
@@ -35,11 +42,11 @@ void setup() {
   Serial.println("Pins set up");
   
   //Reset the z80, leaving it HIGH
-  pinMode(resetPin, OUTPUT);
+  pinMode(RESET_PIN, OUTPUT);
   for (int resetCount = 0; resetCount <3; resetCount++) {
-    digitalWrite(resetPin, LOW);
+    digitalWrite(RESET_PIN, LOW);
     delay(500);
-    digitalWrite(resetPin, HIGH);
+    digitalWrite(RESET_PIN, HIGH);
     delay(500);
   }    
  
@@ -57,13 +64,13 @@ void loop()
     previousMillis = currentMillis;   
 
     // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW)
-      ledState = HIGH;
+    if (clockState == LOW)
+      clockState = HIGH;
     else
-      ledState = LOW;
+      clockState = LOW;
 
     // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
+    digitalWrite(CLOCK_PIN, clockState);
     displayAddress();
     
   }
